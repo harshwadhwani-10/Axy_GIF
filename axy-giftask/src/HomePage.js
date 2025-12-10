bb9a00a2ab59bd29e7477306bacf160aecc01f63
Asodariya Gaurav Bharatbhai <gauravasodariya44@gmail.com>
2025-12-10 18:36:23 +0530

    Update HomePage.js

--- axy-giftask/src/HomePage.js
+++ axy-giftask/src/HomePage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import RandomGif from '../components/RandomGif';
+import RandomGif from './RandomGif';
 
 const HomePage = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
